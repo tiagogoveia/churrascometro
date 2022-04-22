ab8a13ccdc2d7345c5f74452a7c043ae42ef8804
Tiago Goveia <96792189+tiagogoveia@users.noreply.github.com>
2022-04-22 14:04:44 -0300

    04/22

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// carne - 400gr por pessoa + 6h = 650 por pessoal
+//cerveja - 1200ml por pessoa + 6h = 2000ml por pessoal
+//refrigerante/agua - 1000ml por pessoa + 6h = 1500ml por pessoal
+
+//crianças valem por 0,5
+
+
+
+
+
+let inputAdultos = document.getElementById("Adultos");
+let inputCriancas = document.getElementById("Crianças");
+let inputDuracao = document.getElementById("Adultos");
+
+let resultado = document.getElementById("resultado");
+
+function calcular(){
+     
+    console.log("calculando...");
+
+    let Adultos = inputAdultos.ariaValueMax;
+    let Criancas = inputCriancas.ariaValueMax;
+
+    let qdtTotalCarne = 
+
+}
