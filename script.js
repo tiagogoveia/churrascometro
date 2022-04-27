806a012a23a983777c6b5ef6b14d290d19a0c87b
Tiago Goveia <96792189+tiagogoveia@users.noreply.github.com>
2022-04-27 18:25:55 -0300

    27-04-2022

--- script.js
+++ script.js
@@ -18,38 +18,38 @@ function calcular(){
     
 
      let adultos = inputAdultos.value;
-     let criancas = inputCriancas.value;
-     let duracao = inputDuracao.value;
+      let criancas = inputCriancas.value;
+         let duracao = inputDuracao.value;
 
-     let qdtTotalCarne = carnePP(duracao)* adultos + (carnePP(duracao) / 2 * criancas);
+           let qdtTotalCarne = carnePP(duracao)* adultos + (carnePP(duracao) / 2 * criancas);
 
-     let qdtTotalCerveja = cervejaPP(duracao)* adultos;
+            let qdtTotalCerveja = cervejaPP(duracao)* adultos;
 
-     let qdtTotalBebidas= bebidasPP(duracao)* adultos + (bebidasPP(duracao) / 2 * criancas);
+          let qdtTotalBebidas= bebidasPP(duracao)* adultos + (bebidasPP(duracao) / 2 * criancas);
 
 
-    resultado.innerHTML = `<p> ${qdtTotalCarne / 1000} kg de carne.</p>`
-    resultado.innerHTML += `<p> ${Math.ceil(qdtTotalBebidas/2000)} garrafas pet 2L de bebidas em geral. </p>`
-    resultado.innerHTML += `<p> ${Math.ceil(qdtTotalCerveja/355)} latas de cerveja.</p>`
+         resultado.innerHTML = `<p> ${qdtTotalCarne / 1000} kg de carne.</p>`
+       resultado.innerHTML += `<p> ${Math.ceil(qdtTotalBebidas/2000)} garrafas pet 2l de bebidas em geral. </p>`
+     resultado.innerHTML += `<p> ${Math.ceil(qdtTotalCerveja/355)} latas de cerveja.</p>`
      
 
 }
 
 function carnePP(duracao){
 
-    if (duracao >= 6){
-        return 650;
+     if (duracao >= 6){
+         return 650;
         
-    }else {
-        return 400;
-    }
-
+     }else {
+         return 400;
+    
+        }
 }
 
 function cervejaPP(duracao){
 
     if (duracao >= 6){
-        return 2000;
+         return 2000;
         
     }else {
         return 1200;
