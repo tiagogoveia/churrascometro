80abb0feb88ea5912566afb1722ac6a52b00142d
Tiago Goveia <96792189+tiagogoveia@users.noreply.github.com>
2022-04-27 17:53:17 -0300

    04/22

--- script.js
+++ script.js
@@ -7,25 +7,63 @@
 
 
 
-
-
 let inputAdultos = document.getElementById("Adultos");
-let inputCriancas = document.getElementById("Crianças");
-let inputDuracao = document.getElementById("Adultos");
+let inputCriancas = document.getElementById("Criancas");
+let inputDuracao = document.getElementById("Duracao");
 let resultado = document.getElementById("resultado");
 
 
 function calcular(){
 
+    
+
+     let adultos = inputAdultos.value;
+     let criancas = inputCriancas.value;
+     let duracao = inputDuracao.value;
+
+     let qdtTotalCarne = carnePP(duracao)* adultos + (carnePP(duracao) / 2 * criancas);
+
+     let qdtTotalCerveja = cervejaPP(duracao)* adultos;
+
+     let qdtTotalBebidas= bebidasPP(duracao)* adultos + (bebidasPP(duracao) / 2 * criancas);
+
+
+    resultado.innerHTML = `<p> ${qdtTotalCarne / 1000} kg de carne</p>`
+    resultado.innerHTML += `<p> ${qdtTotalBebidas}ml de refrigerante </p>`
+    resultado.innerHTML += `<p> ${qdtTotalCerveja/355} latasde cerveja</p>`
      
-    console.log("calculando...");
 
-    let adultos = inputAdultos.value;
-    let criancas = inputCriancas.Value;
+}
 
+function carnePP(duracao){
 
-    let qdtTotalCarne = 400 * adultos + (400 /2 * criancas);
+    if (duracao >= 6){
+        return 650;
+        
+    }else {
+        return 400;
+    }
 
-    console.log(qdtTotalCarne);
+}
+
+function cervejaPP(duracao){
+
+    if (duracao >= 6){
+        return 2000;
+        
+    }else {
+        return 1200;
+    }
+
+}
+
+function bebidasPP(duracao){
+
+    if (duracao >= 6){
+        return 1500;
+        
+    }else {
+        return 1000;
+    }
 
 }
